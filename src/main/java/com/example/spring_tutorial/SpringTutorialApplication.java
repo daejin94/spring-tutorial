dc0f9f6e55fb7862828c3bf082185c6331c937b7
soleswan <eowls1358@gmail.com>
2024-10-08 17:20:46 +0900

    initial Commit: 초기 셋팅

---
+++ src/main/java/com/example/spring_tutorial/SpringTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialApplication {
+
+	// 어노테이션 및 메인 함수를 통해 시작지점 선언.
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialApplication.class, args);
+	}
+
+}
